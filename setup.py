b26fcc5d2b649fe59fd69db1ddb106fd4a0d7888
Roger D. Serwy <roger.serwy@gmail.com>
2020-07-26 12:42:17 -0500

    xict

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='xict',
+    version='0.0.0',
+    author='Roger D. Serwy',
+    author_email='roger.serwy@gmail.com',
+    url="http://github.com/serwy/xict",
+    packages=['xict'],
+    description='xict',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+)
